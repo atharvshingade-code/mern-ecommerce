8749ed4349958c07978dddb44094afa4e52c280d
atharvshingade-code <atharvshingade7@gmail.com>
2026-03-25 20:22:22 +0530

    mongo db fix

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 console.log("MONGO_URI:", process.env.MONGO_URI);
-
+connectDB();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
